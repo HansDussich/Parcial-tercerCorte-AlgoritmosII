1bc2e9f1b84cb4c88138f4326fe99f0edc7483ff
HansDussich <81448184+HansDussich@users.noreply.github.com>
2023-05-21 13:29:41 -0500

    Add files via upload

---
+++ Parcial_Tercer-Corte/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace Parcial_Tercer_Corte
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDocumento = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.cb_tipo = new System.Windows.Forms.ComboBox();
+            this.btn_agregar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_mostrar = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btn_modificar = new System.Windows.Forms.Button();
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.btn_buscar = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LightSteelBlue;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 260);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(519, 178);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingresa su nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtNombre.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(17, 74);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(124, 22);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtApellido.Location = new System.Drawing.Point(175, 74);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(125, 22);
+            this.txtApellido.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(172, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 14);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ingresa su apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(286, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 14);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ingresa tu fecha de nacimiento";
+            // 
+            // txtDocumento
+            // 
+            this.txtDocumento.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDocumento.Location = new System.Drawing.Point(28, 150);
+            this.txtDocumento.Name = "txtDocumento";
+            this.txtDocumento.Size = new System.Drawing.Size(134, 22);
+            this.txtDocumento.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(318, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(193, 14);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ingresa el tipo de documento";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(25, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(202, 14);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Ingresa numero de documento";
+            // 
+            // dateNacimiento
+            // 
+            this.dateNacimiento.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateNacimiento.Location = new System.Drawing.Point(289, 147);
+            this.dateNacimiento.Name = "dateNacimiento";
+            this.dateNacimiento.Size = new System.Drawing.Size(200, 22);
+            this.dateNacimiento.TabIndex = 11;
+            this.dateNacimiento.Value = new System.DateTime(2023, 5, 20, 13, 12, 3, 0);
+            // 
+            // cb_tipo
+            // 
+            this.cb_tipo.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb_tipo.FormattingEnabled = true;
+            this.cb_tipo.Items.AddRange(new object[] {
+            "Registro civil",
+            "Tarjeta de identidad",
+            "Ceduda de ciudadania",
+            "Cedula de extranjeria",
+            "Pasaporte"});
+            this.cb_tipo.Location = new System.Drawing.Point(341, 74);
+            this.cb_tipo.Name = "cb_tipo";
+            this.cb_tipo.Size = new System.Drawing.Size(135, 22);
+            this.cb_tipo.TabIndex = 12;
+            // 
+            // btn_agregar
+            // 
+            this.btn_agregar.BackColor = System.Drawing.Color.Teal;
+            this.btn_agregar.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_agregar.Location = new System.Drawing.Point(175, 175);
+            this.btn_agregar.Name = "btn_agregar";
+            this.btn_agregar.Size = new System.Drawing.Size(140, 41);
+            this.btn_agregar.TabIndex = 13;
+            this.btn_agregar.Text = "Agregar";
+            this.btn_agregar.UseVisualStyleBackColor = false;
+            this.btn_agregar.Click += new System.EventHandler(this.btn_agregar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(46, 17);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(416, 18);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Bienvenidos, por favor agregar la siguiente informacion";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(8, 34);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(238, 28);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Para ver todos los datos guardados,\r\nhaz clic aqui:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btn_mostrar);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Location = new System.Drawing.Point(537, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(251, 138);
+            this.panel1.TabIndex = 17;
+            // 
+            // btn_mostrar
+            // 
+            this.btn_mostrar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btn_mostrar.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_mostrar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_mostrar.Location = new System.Drawing.Point(66, 74);
+            this.btn_mostrar.Name = "btn_mostrar";
+            this.btn_mostrar.Size = new System.Drawing.Size(125, 39);
+            this.btn_mostrar.TabIndex = 17;
+            this.btn_mostrar.Text = "Mostrar";
+            this.btn_mostrar.UseVisualStyleBackColor = false;
+            this.btn_mostrar.Click += new System.EventHandler(this.btn_mostrar_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.btn_agregar);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.cb_tipo);
+            this.panel2.Controls.Add(this.txtNombre);
+            this.panel2.Controls.Add(this.dateNacimiento);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.txtApellido);
+            this.panel2.Controls.Add(this.txtDocumento);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(12, 23);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(519, 231);
+            this.panel2.TabIndex = 18;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.btn_modificar);
+            this.panel3.Controls.Add(this.btn_eliminar);
+            this.panel3.Controls.Add(this.btn_buscar);
+            this.panel3.Controls.Add(this.txtBuscar);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Location = new System.Drawing.Point(537, 167);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(251, 271);
+            this.panel3.TabIndex = 18;
+            // 
+            // btn_modificar
+            // 
+            this.btn_modificar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btn_modificar.Enabled = false;
+            this.btn_modificar.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_modificar.Location = new System.Drawing.Point(55, 167);
+            this.btn_modificar.Name = "btn_modificar";
+            this.btn_modificar.Size = new System.Drawing.Size(136, 41);
+            this.btn_modificar.TabIndex = 22;
+            this.btn_modificar.Text = "Modificar";
+            this.btn_modificar.UseVisualStyleBackColor = false;
+            this.btn_modificar.Click += new System.EventHandler(this.btn_modificar_Click_1);
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.BackColor = System.Drawing.Color.Brown;
+            this.btn_eliminar.Enabled = false;
+            this.btn_eliminar.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_eliminar.Location = new System.Drawing.Point(55, 214);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(136, 39);
+            this.btn_eliminar.TabIndex = 21;
+            this.btn_eliminar.Text = "Eliminar";
+            this.btn_eliminar.UseVisualStyleBackColor = false;
+            this.btn_eliminar.Click += new System.EventHandler(this.btn_eliminar_Click);
+            // 
+            // btn_buscar
+            // 
+            this.btn_buscar.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btn_buscar.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_buscar.Location = new System.Drawing.Point(55, 102);
+            this.btn_buscar.Name = "btn_buscar";
+            this.btn_buscar.Size = new System.Drawing.Size(136, 38);
+            this.btn_buscar.TabIndex = 20;
+            this.btn_buscar.Text = "Buscar";
+            this.btn_buscar.UseVisualStyleBackColor = false;
+            this.btn_buscar.Click += new System.EventHandler(this.btn_buscar_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBuscar.Location = new System.Drawing.Point(45, 74);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(163, 22);
+            this.txtBuscar.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(20, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(212, 42);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Para eliminar o modificar alguna \r\ninformacion, por favor ingresa \r\nel numero de " +
+    "identificacion\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDocumento;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateNacimiento;
+        private System.Windows.Forms.ComboBox cb_tipo;
+        private System.Windows.Forms.Button btn_agregar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_mostrar;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btn_buscar;
+        private System.Windows.Forms.Button btn_eliminar;
+        private System.Windows.Forms.Button btn_modificar;
+    }
+}
+
---
+++ Parcial_Tercer-Corte/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Parcial_Tercer_Corte
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // conexion a la base de datos
+        SqlConnection conexion = new SqlConnection("server=DESKTOP-A38J5RV ; database=db-parcial ; integrated security = true");
+
+        // limpiar el formulario
+        public void formularioBlanco()
+        {
+            txtNombre.Clear();
+            txtApellido.Clear();
+            cb_tipo.SelectedIndex = -1;
+            txtDocumento.Clear();
+            dateNacimiento.ResetText();
+        }
+
+        //boton de agregar a la base de datos
+        private void btn_agregar_Click(object sender, EventArgs e)
+        {
+            conexion.Open();
+
+            try
+            {
+
+                if (string.IsNullOrEmpty(txtNombre.Text) || string.IsNullOrEmpty(txtApellido.Text) ||
+                    string.IsNullOrEmpty(cb_tipo.Text) || string.IsNullOrEmpty(txtDocumento.Text) ||
+                    string.IsNullOrEmpty(dateNacimiento.Text))
+                {
+                    throw new Exception("Hay campos vacios, por favor completar");
+                }
+                else
+                {
+                    try
+                    {
+                        string cadena = "insert into tb_datos (nombre, apellido, tipo_documento, numero_documento, fecha_nacimiento)" +
+                        " values ('" + txtNombre.Text + "', '" + txtApellido.Text + "', '" + cb_tipo.SelectedItem.ToString() + "', '" + txtDocumento.Text + "', '" + dateNacimiento.Text + "')";
+
+
+                        SqlCommand comando = new SqlCommand(cadena, conexion);
+
+                        comando.ExecuteNonQuery();
+                        MessageBox.Show("La informacion ha sido guardada exitosamente");
+
+                        formularioBlanco();
+
+                        conexion.Close();
+                    }
+                    catch (System.Data.SqlClient.SqlException SqlEx)
+                    {
+                        MessageBox.Show("El numero de identificacion ya existe, verifica por favor");
+                    }
+
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hay campos vacios, por favor completar el formulario");
+            }
+  
+        }
+
+        // Boton de mostrar tabla en el formulario
+        private void btn_mostrar_Click(object sender, EventArgs e)
+        {
+            conexion.Open();
+
+            string mostrar = "select * from tb_datos";
+
+            SqlDataAdapter adaptador = new SqlDataAdapter(mostrar, conexion);
+
+            DataTable tabla = new DataTable();
+            adaptador.Fill(tabla);
+            dataGridView1.DataSource = tabla;
+            SqlCommand comando = new SqlCommand();
+
+            
+
+            conexion.Close();
+        }
+
+        //Boton para buscar un elemento
+        private void btn_buscar_Click(object sender, EventArgs e)
+        {
+            
+            conexion.Open();
+
+            txtDocumento.Enabled = false;
+
+            string texto = txtBuscar.Text;
+            string cadena = "select * from tb_datos where numero_documento='" + texto + "'";
+
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            SqlDataReader reader = comando.ExecuteReader();
+
+            if (reader.Read())
+            {
+                txtNombre.Text = reader["nombre"].ToString();
+                txtApellido.Text = reader["apellido"].ToString();
+                cb_tipo.Text = reader["tipo_documento"].ToString();
+                txtDocumento.Text = reader["numero_documento"].ToString();
+                dateNacimiento.Text = reader["fecha_nacimiento"].ToString();
+
+
+                reader.Close(); 
+
+                string mostrar = "select * from tb_datos where numero_documento='" + txtBuscar.Text + "'";
+                SqlDataAdapter adaptador = new SqlDataAdapter(mostrar, conexion);
+
+                DataTable tabla = new DataTable();
+
+                adaptador.Fill(tabla);
+                dataGridView1.DataSource = tabla;
+
+                btn_eliminar.Enabled = true;
+                btn_modificar.Enabled = true;
+
+                
+            }
+            else
+            {
+                MessageBox.Show("Numero de documento no encontrado, por favor verificar");
+            }
+
+            conexion.Close();
+        }
+
+        //Boton para modificar o actualizar un elemento
+        private void btn_modificar_Click_1(object sender, EventArgs e)
+        {
+            conexion.Open();
+            
+            string num = txtBuscar.Text;
+
+            string cadena = "UPDATE tb_datos SET nombre = @nombre, apellido = @apellido, tipo_documento = @tipo, fecha_nacimiento = @fecha WHERE numero_documento = @numero_documento";
+
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            comando.Parameters.AddWithValue("@nombre", txtNombre.Text);
+            comando.Parameters.AddWithValue("@apellido", txtApellido.Text);
+            comando.Parameters.AddWithValue("@tipo", cb_tipo.SelectedItem.ToString());
+            comando.Parameters.AddWithValue("@fecha", dateNacimiento.Text);
+            comando.Parameters.AddWithValue("@numero_documento", num);
+
+            int cant;
+            cant = comando.ExecuteNonQuery();
+
+            if (cant == 1)
+            {
+                MessageBox.Show("Se ha actualizado exitosamente");
+                txtDocumento.Enabled = true;
+                formularioBlanco();
+                btn_modificar.Enabled=false;
+                btn_eliminar.Enabled = false;
+
+            }
+            else
+            {
+                MessageBox.Show("No se pudo actualizar");
+            }
+
+            conexion.Close();
+
+        }
+
+        //Bonton para eliminar un elemento
+        private void btn_eliminar_Click(object sender, EventArgs e)
+        {
+
+            conexion.Open();
+
+            string texto = txtBuscar.Text;
+
+            string cadena = "delete from tb_datos where numero_documento='" + texto + "'";
+
+            SqlCommand comando = new SqlCommand( cadena, conexion);
+
+            int cant;
+
+            cant = comando.ExecuteNonQuery();
+            if (cant == 1)
+            {
+                MessageBox.Show("Se ha elimina exitosamente");
+                txtDocumento.Enabled = true;
+                formularioBlanco();
+                btn_modificar.Enabled=false;
+                btn_eliminar.Enabled=false;
+
+            }
+            else
+            {
+                MessageBox.Show("No se ha podido eliminar ");
+            }
+            conexion.Close();
+        }
+    }
+}
